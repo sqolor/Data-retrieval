b38d17123f5d175a7d34c668998f2f41b1d510cf
MikeyGh <mikey997@gmail.com>
2020-01-26 11:35:49 +0200

    Added Geomap and tweetmap

--- InformationRetrievalProj.py
+++ InformationRetrievalProj.py
@@ -2,11 +2,13 @@
 """
 Created on Mon Jan 13 20:23:23 2020
 
-@author: benic
+@author: benic and mikeyg
 """
 
 import pandas as pd
 import os
+
+import prep as prep
 from nltk.tokenize import TweetTokenizer
 import nltk
 import re
@@ -39,11 +41,15 @@ worksheet = workbook['sheet']
 fileList =  (list(iter_rows(worksheet)))
 geoLocation = []
 tweet = []
-
+j=2
+tweetmap=dict()
+geomap=dict()
 for col in fileList:
     geoLocation.append(col[5])#1 is column index
     tweet.append(col[6])#2 is column index
-
+    tweetmap[j]=col[6]
+    geomap[j]=col[5]
+    j=j+1
 counter = 2
 for i in tweet:
     terms = prep.ngram_tokenizer(text=i)
