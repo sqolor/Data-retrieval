c34c701ace768e0b065ace62c8da6b1dff3f2237
Beni Cherf <benich2194@gmail.com>
2020-02-25 23:08:33 +0200

    added sentimental glossary, updated code

--- InformationRetrievalProj.py
+++ InformationRetrievalProj.py
@@ -2,30 +2,46 @@
 """
 Created on Mon Jan 13 20:23:23 2020
 
-@author: benic and mikeyg
+@author: benic
 """
 
 import pandas as pd
 import os
-
-import prep as prep
 from nltk.tokenize import TweetTokenizer
 import nltk
 import re
 nltk.download('stopwords')
+nltk.download('words')
 from nltk.corpus import stopwords
 from openpyxl import load_workbook
 from nltk.stem import PorterStemmer
 from spellchecker import SpellChecker
+import xlsxwriter 
+import os.path
+from os import path
+
+english_vocab = set(w.lower() for w in nltk.corpus.words.words())
 
 spell = SpellChecker()
-
+sentimental_dictionary={}
 tknzr = TweetTokenizer(preserve_case=True, reduce_len=True, strip_handles=True)
 ps = PorterStemmer()
 cachedStopWords = stopwords.words("english")
+import csv
+
+
+
+row = 0
+column = 0
+bool createdGlossary = False
+if(path.exists("Sentimental Glossary.xlsx")):
+    createdGlossary = True
+else:
+    workbook_glossary = xlsxwriter.Workbook('Sentimental Glossary.xlsx') 
+    worksheet_glossary = workbook_glossary.add_worksheet() 
 
 dictionary = {}
-
+vocabulary = dict()
 def iter_rows(ws):
     result=[]
     for row in ws.iter_rows():
@@ -35,29 +51,33 @@ def iter_rows(ws):
         result.append(rowlist)
     return result
 
+def word_count(str):
+
+    for word in str:
+        if word in vocabulary:
+            vocabulary[word] += 1
+        else:
+            vocabulary[word] = 1
 workbook = load_workbook(filename='data.xlsx', read_only = True)
 worksheet = workbook['sheet']
 
 fileList =  (list(iter_rows(worksheet)))
 geoLocation = []
 tweet = []
-j=2
-tweetmap=dict()
-geomap=dict()
+
 for col in fileList:
     geoLocation.append(col[5])#1 is column index
     tweet.append(col[6])#2 is column index
-    tweetmap[j]=col[6]
-    geomap[j]=col[5]
-    j=j+1
+
 counter = 2
 for i in tweet:
-    terms = prep.ngram_tokenizer(text=i)
-    print(terms)
     currentTweet = tknzr.tokenize(i.casefold())
     currentTweet = [word for word in currentTweet if word not in cachedStopWords]
+    currentTweet = [word for word in currentTweet if word in english_vocab]
     currentTweet = [s.strip('.') for s in currentTweet] 
     currentTweet = [s.replace('.', '') for s in currentTweet]
+    currentTweet = [s.strip('#') for s in currentTweet] 
+    currentTweet = [s.replace('#', '') for s in currentTweet]
     currentTweet = [s.strip(':') for s in currentTweet] 
     currentTweet = [s.replace(':', '') for s in currentTweet]
     currentTweet = [s.strip('!') for s in currentTweet] 
@@ -93,12 +113,17 @@ for i in tweet:
     currentTweet = filter (lambda i: len (i) > 2, currentTweet)
     currentTweet = list(filter(None, currentTweet))
     currentTweet = [spell.correction(i) for i in currentTweet]
-    
     currentTweet = [ps.stem(i) for i in currentTweet]
-    
-    
+    word_count(currentTweet)
     print(currentTweet)
-    
+
+
+print(vocabulary)
+if(createdGlossary is False):
+    for key in vocabulary.keys():
+    worksheet_glossary.write(row, column, key) 
+    row+=1
 
     
 
+workbook_glossary.close() 
